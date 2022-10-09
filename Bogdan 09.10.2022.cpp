3a0686aaa23a791cadf17dce899c42d365e4d200
Morti123 <114169876+Morti123@users.noreply.github.com>
2022-10-09 18:24:23 +0300

    Add files via upload

---
+++ Bogdan 09.10.2022.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+   // setlocale(LC_ALL, "Russian");
+   // n1
+   /*int hp;
+    cin >> hp;
+    bool r = hp > 0;
+    cout << boolalpha << r << endl;*/
+  //  n2
+   /* int t;
+    cin >> t;
+    if (t > 20) {
+        cout << "on" << endl;
+    }
+    if (t <= 20) {
+        cout << "off" << endl;
+    }*/
+   // n3
+  /*int T, t, v;
+    cin >> T >> t >> v;
+    if (v > 80 || T > t) {
+        cout << "off" << endl;
+    }
+    else if (v <= 80 && t > T) {
+        cout << "on" << endl;
+    }*/
+   // n4
+   /* double x, y;
+    cin >> x;
+    if ( x > 0) {
+        y = sqrt(x - 2);
+        cout << y << endl;
+   }
+    else if (x == 0) {
+        y = 0;
+        cout << y << endl;
+    }
+    else if (x < 0) {
+        y = abs(x);
+        cout << y << endl;
+    }*/
+   // n5
+   /* int p;
+    cin >> p;
+    switch (p) {
+    case 1: cout << "большой палец" << endl; break;
+    case 2: cout << "указательный палец" << endl; break;
+    case 3: cout << "средний палец" << endl; break;
+    case 4: cout << "безымянный палец" << endl; break;
+    case 5: cout << "мизинец" << endl; break;
+    default: cout << "ошибка" << endl; break;
+    }*/
+   // n6
+   /* int t, nt;
+    cin >> t;
+    cin >> nt;
+     if (t > 20 ) {
+        cout << "ac on" << endl;
+    }
+    else if (t <= 20) {
+        cout << "ac off" << endl;
+    }
+    else if (nt < t) {
+        cout << "open window" << endl;*/
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
